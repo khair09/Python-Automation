143dd34107fa8efc2f6fe7a4068ea1505f4e732c
khair09 <81907639+khair09@users.noreply.github.com>
2021-04-04 17:21:33 +0600

    Add files via upload

---
+++ Test1.py
@@ -0,0 +1,19 @@
+"""
+Test program:
+r=3, C=4
+****
+****
+****
+
+"""
+r=int(input())
+c=int(input())
+
+row=0
+while row<r:
+    column = 0
+    while column<c:
+        print('*',end=" ")
+        column+=1
+    row+=1
+    print("")
